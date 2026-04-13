7427caf6dcd5588234977d5b235bdf179174506f
oOtroyOo <497790325@qq.com>
2026-04-13 11:53:59 +0800

    1

---
+++ valaxy.config.ts
@@ -0,0 +1,21 @@
+// import type { UserThemeConfig } from 'valaxy-theme-sakura'
+import { defineValaxyConfig } from 'valaxy'
+import type { ThemeUserConfig } from 'valaxy-theme-sakura'
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+
+export default defineValaxyConfig<ThemeUserConfig>({
+  // site config see site.config.ts
+
+  theme: 'sakura',
+
+  themeConfig: {},
+
+  unocss: { safelist },
+})
